662588f12f0384bdfb9a458f9b8f35d8e3aa5ba0
José Renato <259754904+underscorejoser@users.noreply.github.com>
2026-05-16 17:18:42 -0300

    prefer nvidia-container-toolkit from nvidia

--- files/scripts/installnvidiapackages.sh
+++ files/scripts/installnvidiapackages.sh
@@ -6,7 +6,6 @@
 
 set -euo pipefail
 
-declare -a nvidia_packages_list=('libva-nvidia-driver' 'nvidia-container-toolkit')
 declare -a nvidia_desktop_packages_list+=(
     'libnvidia-fbc' 'nvidia-driver' 'nvidia-modprobe' 'nvidia-persistenced' 'nvidia-settings'
 )
@@ -14,20 +13,21 @@ declare -a nvidia_desktop_packages_list+=(
 is_desktop='false'
 [[ "${IMAGE_NAME}" != *'securecore'* && "${IMAGE_NAME}" != *'iot'* ]] && is_desktop='true'
 
-declare -a packages_to_install=("${nvidia_packages_list[@]}")
+declare -a nvidia_packages_list=('libva-nvidia-driver')
 if [[ "${IMAGE_NAME}" == *open* ]]; then
-    packages_to_install+=('nvidia-driver-cuda')
-    [[ "${is_desktop}" == 'true' ]] && packages_to_install+=("${nvidia_desktop_packages_list[@]}")
+    nvidia_packages_list+=('nvidia-driver-cuda')
+    [[ "${is_desktop}" == 'true' ]] && nvidia_packages_list+=("${nvidia_desktop_packages_list[@]}")
 else
-    packages_to_install+=('nvidia-driver-580xx-cuda')
-    [[ "${is_desktop}" == 'true' ]] && packages_to_install+=("${nvidia_desktop_packages_list[@]/%/-580xx}")
+    nvidia_packages_list+=('nvidia-driver-580xx-cuda')
+    [[ "${is_desktop}" == 'true' ]] && nvidia_packages_list+=("${nvidia_desktop_packages_list[@]/%/-580xx}")
 fi
 
 dnf install -y --setopt=install_weak_deps=False \
     --enable-repo='terra-nvidia' \
-    --enable-repo='nvidia-container-toolkit' \
     --disable-repo='fedora-multimedia' \
-    "${packages_to_install[@]}"
+    "${nvidia_packages_list[@]}"
+
+dnf install -y --setopt=install_weak_deps=False --enable-repo='nvidia-container-toolkit' nvidia-container-toolkit
 
 kmod_version=$(rpm -qa | grep akmod-nvidia | awk -F':' '{print $(NF)}' | awk -F'-' '{print $(NF-1)}')
 terra_version=$(rpm -qa | grep nvidia-modprobe | awk -F':' '{print $(NF)}' | awk -F'-' '{print $(NF-1)}')
